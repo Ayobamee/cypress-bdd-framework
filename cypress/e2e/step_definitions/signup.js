acc1b18d0d7fd5459ec7ba07c6445748e2795d03
Ayobameee <ayobami@affinityclick.com>
2023-08-03 10:30:40 +0100

    added test data to the fixtures file

--- cypress/e2e/step_definitions/signup.js
+++ cypress/e2e/step_definitions/signup.js
@@ -1,10 +1,12 @@
 import { Given, When } from "@badeball/cypress-cucumber-preprocessor";
-import { loginPage } from "@pages/LoginPage";
 import { signUpPage } from "@pages/SignUpPage";
 
 Given("I am on the sign up page", () => {
-  cy.visit("/register");
-  signUpPage.submitSignUp("ayo+77779@affinityclick.com", "Affinityclick@1");
+  cy.fixture("testData").then((users) => {
+    const testUser = users.userDetails;
+    cy.visit("/register");
+    signUpPage.submitSignUp(testUser.username, testUser.password);
+  });
 });
 
 When("I agree to sign up", () => {
